f796c920f9e40a2fb276d8d02be9ed75a84baee6
Ivo <ivo@amie.so>
2026-02-08 20:05:07 +0100

    First commit

---
+++ records/recordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  recordsApp.swift
+//  records
+//
+//  Created by Ivo Silva on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct recordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
